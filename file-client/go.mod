f1938cad52b26866a85c47bc390b26890e755071
tanxianchen <136929780@qq.com>
2022-06-17 10:35:32 +0800

    更新上传 和 客户端

---
+++ file-client/go.mod
@@ -0,0 +1,36 @@
+module file-client
+
+go 1.17
+
+require (
+	github.com/fsnotify/fsnotify v1.5.4
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
+	github.com/sirupsen/logrus v1.8.1
+	github.com/spf13/viper v1.12.0
+	google.golang.org/grpc v1.47.0
+	google.golang.org/protobuf v1.28.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/lestrrat-go/strftime v1.0.6 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220519153652-3a47de7e79bd // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
+)
---
+++ file-client/utils/grpcClient/grpcClient.go
@@ -0,0 +1,65 @@
+package grpcClient
+
+import (
+	"context"
+	"file-client/proto/newvcms"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"strconv"
+)
+
+func GetGrpcClient(host string, port int) *grpc.ClientConn {
+	var serviceHost = host + ":" + strconv.Itoa(port)
+	conn, err := grpc.Dial(serviceHost, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil
+	}
+	return conn
+}
+
+func AddFileInstence(host string, port int, request *newvcms.InstanceCreateRequest) (*newvcms.CreateResult, error) {
+	conn := GetGrpcClient(host, port)
+	defer conn.Close()
+	grpcClient := newvcms.NewInstanceServiceClient(conn)
+	rsp, err := grpcClient.CreateInstance(context.TODO(), request)
+	if err != nil {
+		return nil, err
+	}
+	return rsp, nil
+}
+
+func DelFileInstence(host string, port int, request *newvcms.InstanceDeleteRequest) (*newvcms.DeleteResult, error) {
+	conn := GetGrpcClient(host, port)
+	defer conn.Close()
+	grpcClient := newvcms.NewInstanceServiceClient(conn)
+	rsp, err := grpcClient.DeleteInstance(context.TODO(), request)
+	if err != nil {
+		return nil, err
+	}
+	return rsp, nil
+}
+
+func FindInstenceById(host string, port int) (*newvcms.InstanceFindByIdResponse, error) {
+	conn := GetGrpcClient(host, port)
+	defer conn.Close()
+	grpcClient := newvcms.NewInstanceServiceClient(conn)
+	rsp, err := grpcClient.FindInstanceById(context.TODO(), &newvcms.InstanceFindByIdRequest{
+		Uid:         "",
+		Identifiers: nil,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return rsp, nil
+}
+
+func FindInstance(host string, port int, request *newvcms.InstanceFindRequest) (*newvcms.InstanceFindResponse, error) {
+	conn := GetGrpcClient(host, port)
+	defer conn.Close()
+	grpcClient := newvcms.NewInstanceServiceClient(conn)
+	rsp, err := grpcClient.FindInstance(context.TODO(), request)
+	if err != nil {
+		return nil, err
+	}
+	return rsp, nil
+}
